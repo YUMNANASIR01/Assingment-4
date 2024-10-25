3e6a864648bc4ef4804bbcb407dfcf48d16036fb
YUMNANASIR01 <yumnanasir719@gmail.com>
2024-10-25 03:20:40 -0700

    second

--- components/blogs.tsx
+++ components/blogs.tsx
@@ -8,7 +8,7 @@ function Blogs() {
        <h2 className='recent'>Recent post</h2>
        <h2 className='view'>view all</h2>
        </div>
-       <div className='div1'>
+       <div className='div1 '>
        <div className= 'div2 '> <h1>Making a design system from scratch</h1>
           <br />
           <h4>12 feb 2020  &nbsp; &nbsp;  | &nbsp; &nbsp;  Design, Pattern</h4> <br />
